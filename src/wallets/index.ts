3201bc836a02e647144a6f81b8968bdc1ef34fb2
Ruveyda Kışla <thisisruveyda@gmail.com>
2025-10-28 10:32:13 +0300

    fix: reposition wallet suggetsion

--- src/wallets/index.ts
+++ src/wallets/index.ts
@@ -12,14 +12,14 @@ import { walletConnectConfig } from './walletConnect';
 import { ledgerConfig } from './ledger';
 
 export const walletsConfig: Record<SupportedWallet, IWallet> = {
-  [SupportedWallet.Hana]: hanaConfig,
-  [SupportedWallet.Rabet]: rabetConfig,
-  [SupportedWallet.Lobstr]: lobstrConfig,
   [SupportedWallet.Freighter]: freighterConfig,
+  [SupportedWallet.Rabet]: rabetConfig,
   [SupportedWallet.WalletConnect]: walletConnectConfig,
   [SupportedWallet.Xbull]: xBullConfig,
-  [SupportedWallet.Albedo]: albedoConfig,
   [SupportedWallet.Hot]: hotConfig,
-  [SupportedWallet.Klever]: kleverConfig,
+  [SupportedWallet.Hana]: hanaConfig,
+  [SupportedWallet.Lobstr]: lobstrConfig,
   [SupportedWallet.Ledger]: ledgerConfig,
+  [SupportedWallet.Albedo]: albedoConfig,
+  [SupportedWallet.Klever]: kleverConfig,
 };
