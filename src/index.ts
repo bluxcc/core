57d5cb241e70fd1cefa0f05778bfbe8c8b3729c1
Ceyda Esen <ceydae317@gmail.com>
2026-05-12 14:29:21 +0300

    chore: automatically login the user after refresh

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
+import { _login } from './exports/blux';
+import { getState } from './store';
+import { timeout } from './utils/helpers';
 import { preloadLogos } from './utils/preloadImages';
 
 export * from './exports';
 export { createConfig } from './exports/createConfig';
 
 preloadLogos();
+
+(async () => {
+  while (true) {
+    const s = getState();
+
+    if (s.authState.isReady) break;
+
+    await timeout(50);
+  }
+
+  _login(true);
+})();
